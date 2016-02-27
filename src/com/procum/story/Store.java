a1fe730e3ac61ca4c4442251d3ff665cb693a0df
wei531011173 <weiyonggame@qq.com>
2016-02-27 21:52:27 +0800

    add story

--- src/com/procum/story/Store.java
+++ src/com/procum/story/Store.java
@@ -9,9 +9,34 @@ public class Store {
 	
 	List<Integer> products = new ArrayList<Integer>();
 	
-	public void get(){}
+	public synchronized void get(){
+		
+		try {
+			if(this.products.size() <= 0)
+				this.wait();
+			int p = this.products.remove(0);
+			System.out.println("consume:" + p);
+			this.notifyAll();
+			Thread.sleep(200);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
 	
-	public void put(){}
+	public synchronized void put(){
+		
+		try {
+			if(this.products.size() >= MAX_SIZE)
+				this.wait();
+			int p = products.size() + 1;
+			this.products.add(p);
+			System.out.println("produce:" + p);
+			this.notifyAll();
+			Thread.sleep(200);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
 	
 
 }
